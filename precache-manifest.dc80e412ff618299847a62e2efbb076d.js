f7dd32f144e975f3b20ebd18a8e1bd0e97116226
David Jones <kingdavidjones@Davids-MacBook-Pro.local>
2020-08-14 12:26:03 -0600

    Updates

---
+++ precache-manifest.dc80e412ff618299847a62e2efbb076d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e70693bb2828dfc531d2de8390e088a3",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "b7a42c0a617a16f2f1af",
+    "url": "/movie-app/static/js/2.2fb31a43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.2fb31a43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd73f8deb2a6204d2ac8",
+    "url": "/movie-app/static/js/main.81d1eb2b.chunk.js"
+  },
+  {
+    "revision": "f06c81a5755dd889799e",
+    "url": "/movie-app/static/js/runtime-main.16741050.js"
+  }
+]);
